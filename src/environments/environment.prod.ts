eccaefe0ebf53dde1d60b9953509c203ad2f66be
helmibaharin <helmibaharin@gmail.com>
2022-08-30 17:08:59 +0800

    ksjfk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  api: 'https://topwebsitehelmi.herokuapp.com:5000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'https://localhost:5000'
+  api: 'https://topwebsitehelmi.herokuapp.com:5000'
 };
 
 /*
